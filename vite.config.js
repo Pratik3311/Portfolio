95f04b01b76b7a9d64e2ca9073ba76030927dd54
Pratik3311 <pratikgaikwad1133@gmail.com>
2024-12-19 23:55:20 +0530

    Updates in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',  // Ensure this is set to 'dist' or the folder you're using
+  },
   plugins: [react()],
 })
